b370382c8c8b30ea36417e3e1b9b624c3bbfa458
Chackochan Boby <chackoakasalayil@gmail.com>
2024-09-26 13:43:06 +0530

    commented out some lines from vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,11 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    https: {
-      key: './.cert/key.pem',
-      cert: './.cert/cert.pem',
-    }
-  }
+  // plugins: [react()],
+  // server: {
+  //   https: {
+  //     key: './.cert/key.pem',
+  //     cert: './.cert/cert.pem',
+  //   }
+  // }
 })
