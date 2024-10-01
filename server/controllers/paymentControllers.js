2b3b88880030403bc0753c016b3b57fe1ec0f8e8
Chackochan Boby <chackoakasalayil@gmail.com>
2024-10-01 12:25:41 +0530

    more console logs for debugging

--- server/controllers/paymentControllers.js
+++ server/controllers/paymentControllers.js
@@ -2,6 +2,7 @@ const { Enrollment } = require('../models/enrollmentModel');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const webhook = async (request, response) => {
+  console.log(request.body)
   console.log("Raw Body:", request.body.toString()); // Add this line for debugging
   let event
   const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET_KEY;
